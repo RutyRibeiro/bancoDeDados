51d7789cd635b07417b2be56433d83837c32b66d
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-01-11 14:46:08 -0300

    Update comparação.py

--- banco de dados/comparação.py
+++ banco de dados/comparação.py
@@ -2,15 +2,15 @@ import mysql.connector
 import csv
 lista = []
 with open('escolas.csv', encoding='utf-8', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
     for row in spamreader:
         dicionario={}
         dicionario["grupo"]="A1_GRPECON = '{}'".format(row[0].strip())
         dicionario["email"]=row[1].strip()
 
         lista.append(dicionario)
-print(lista)
 
+print(lista)
 
 config = {
     'host': '',
@@ -23,7 +23,7 @@ config = {
 def select():
     try:
         conn = mysql.connector.connect(**config)
-        print("Acesso ao banco de dados: Conexão Estabelecida - INSERT")
+        print("Acesso ao banco de dados: Conexão Estabelecida ")
     except mysql.connector.Error as err:
         print(err)
     else:
@@ -32,11 +32,16 @@ def select():
             for i, row in enumerate(lista):
                 
                 buscaDados = """SELECT USR_VINCULO, USR_EMAIL FROM EasyBI.usuarios usr WHERE USR_CODCLI = 3284 AND USR_EMAIL= '%s'"""
-                cursor.execute(buscaDados % (row['email']))
+                email=row['email']
+                cursor.execute(buscaDados % (email))
                 resul = cursor.fetchall()
                 
-                if (row['grupo']!=resul[0][0]):
-                    print (f'\033[33m olhe : {resul[0][1]}\033[m')
+                if (resul):
+                    if (row['grupo']!=resul[0][0]):
+                        print (f'\033[33m olhe : {resul[0][1]}\033[m')
+                else:
+                    print(f'{email} não existe!')
+
         except Exception as e:
             print(e)
         finally:           
@@ -44,6 +49,6 @@ def select():
     
             conn.commit()
             conn.close()
-            print("Fechamento do banco de dados: Com sucesso - INSERT")
+            print("Fechamento do banco de dados: Com sucesso")
 select()
 
